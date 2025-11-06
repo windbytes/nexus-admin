baf1f407bd2e18a7e02e78d1846af9ac700fd1ed
ycl <499475142@qq.com>
2025-11-06 17:43:41 +0800

    ✨ refactor(Workflow): enhance the Workflow component by integrating FreeLayoutEditorProvider, updating layout structure, and improving toolbar organization for a more intuitive user experience.

--- src/views/integrated/Workflow/FlowEditor.tsx
+++ src/views/integrated/Workflow/FlowEditor.tsx
@@ -7,11 +7,11 @@ import { useEffect } from 'react';
  * 流程编辑器
  * @returns
  */
-const Workflow: React.FC = () => {
+const WorkflowEditor: React.FC = () => {
   // 获取主题配置
   const colorPrimary = usePreferencesStore((state) => state.preferences.theme.colorPrimary);
   // 获取路由参数（应用ID）
-  const { appId } = useParams({ from: '/_authenticated/integrated/app/$appId/workflow' });
+  const appId = useParams({ from: '/_authenticated/integrated/app/$appId/workflow' });
 
   // 路由跳转
   const navigate = useNavigate();
@@ -27,30 +27,25 @@ const Workflow: React.FC = () => {
   };
 
   return (
-    <div className="workflow-container relative h-full w-full min-w-[960px] min-h-[600px]">
-      <FreeLayoutEditorProvider>
+    <FreeLayoutEditorProvider>
+      <div className="workflow-container relative h-full w-full min-w-[960px] min-h-[600px]">
         {/* 左边工具栏 */}
-        <div className="absolute pointer-events-none left-0 top-0 z-10 flex h-full w-12 items-center justify-center p-1 pl-2">
+        <div className="absolute pointer-events-none left-0 top-0 z-10 flex w-12 items-center justify-center p-1 pl-2">
           <div className="pointer-events-auto flex flex-col items-center rounded-lg border-[0.5px] bg-white p-0.5 text-gray-300 shadow-lg">
             左侧工具栏
           </div>
         </div>
         {/* 底部工具栏 */}
-        <div className="absolute bottom-0 left-0 right-0 z-10 px-1 w-full flex items-center justify-between">
-          <div>撤销、重做（变更历史）</div>
-          <div>缩略图</div>
-        </div>
+        <div className="absolute bottom-0 left-0 right-0 z-10 px-1 w-auto">撤销、重做（变更历史）、缩略图</div>
         {/* 顶部工具栏 */}
-        <div className="absolute top-2 left-0 right-0 z-10 px-3 h-0 w-full flex items-center justify-between">
-          <div>自动保存</div>
-          <div>项目名称</div>
-          <div>操作按钮 历史记录（版本历史）</div>
+        <div className="absolute top-7 left-0 right-0 z-10 px-3 h-0 w-full items-center justify-center">
+          自动保存提示+操作按钮+历史记录（版本历史）
         </div>
         {/* 画布 */}
-        <div className="workflow-canvas relative w-full h-full overflow-hidden z-0">这里显示画布，流程ID ：{appId}</div>
-      </FreeLayoutEditorProvider>
-    </div>
+        <div className="workflow-canvas relative w-full h-full overflow-hidden z-0">这里显示画布，流程ID {appId}</div>
+      </div>
+    </FreeLayoutEditorProvider>
   );
 };
 
-export default Workflow;
+export default WorkflowEditor;
