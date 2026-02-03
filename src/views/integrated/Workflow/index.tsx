10399a540e138349f70c2ec72033731c18a78fe5
ycl <499475142@qq.com>
2026-02-03 11:44:33 +0800

    feat: 添加工作流编辑器功能，集成 @xyflow/react 以支持节点和边的动态管理

--- src/views/integrated/Workflow/index.tsx
+++ src/views/integrated/Workflow/index.tsx
@@ -1,15 +1,79 @@
 import { useParams } from '@tanstack/react-router';
 import './workflow.scss';
+import {
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+  type DefaultEdgeOptions,
+  type Edge,
+  type FitViewOptions,
+  type Node,
+  type OnConnect,
+  type OnEdgesChange,
+  type OnNodeDrag,
+  type OnNodesChange,
+  ReactFlow,
+  ReactFlowProvider,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import { useCallback, useState } from 'react';
+
+const initialNodes: Node[] = [
+  { id: '1', data: { label: 'Node 1' }, position: { x: 5, y: 5 } },
+  { id: '2', data: { label: 'Node 2' }, position: { x: 5, y: 100 } },
+];
+
+const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
+
+const defaultEdgeOptions: DefaultEdgeOptions = {
+  animated: true,
+};
+
+const onNodeDrag: OnNodeDrag = (_, node) => {
+  console.log('drag event', node.data);
+};
 
 /**
  * 流程编辑器
  * @returns
  */
 const Workflow: React.FC = () => {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
   // 当前应用ID
   const { appId } = useParams({ from: '/nexus/integrated/app/$appId/workflow' });
 
-  return <div className="workflow-feature-overview -m-2">流程编排{appId}</div>;
+  const onNodesChange: OnNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange: OnEdgesChange = useCallback(
+    (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect: OnConnect = useCallback((connection) => setEdges((eds) => addEdge(connection, eds)), [setEdges]);
+
+  return (
+    <ReactFlowProvider>
+      <div className="workflow-feature-overview -m-2">
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          onNodeDrag={onNodeDrag}
+          fitView
+          fitViewOptions={fitViewOptions}
+          defaultEdgeOptions={defaultEdgeOptions}
+        />
+      </div>
+    </ReactFlowProvider>
+  );
 };
 
 export default Workflow;
